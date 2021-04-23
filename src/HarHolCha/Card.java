939ad6aff912388fdd08ed0ab59c78cffaecd336
jenicekhartigu <honza.hartig@seznam.cz>
2021-04-23 12:47:10 +0200

    first commit

---
+++ src/HarHolCha/Card.java
@@ -0,0 +1,32 @@
+package HarHolCha;
+
+public class Card {
+    //data
+    private String value;
+    private String symbol;
+    private int points;
+
+
+    //konstruktor
+    public Card(String value, String symbol, int points){
+        this.value = value;
+        this.symbol = symbol;
+        this.points = points;
+    }
+    
+    public String getValue(){
+        return value;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+    @Override
+    public String toString() {
+        return "Card [symbol=" + symbol + ", value=" + value + "]";
+    }
+}
