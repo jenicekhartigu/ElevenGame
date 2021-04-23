d65493cc19c049ab30f6935dcef97a0af0807bbd
FilipCharouzd <filip.charouzd@tul.cz>
2021-04-23 12:28:03 +0200

    Update Card.java

--- src/gameTools/Card.java
+++ src/gameTools/Card.java
@@ -10,5 +10,34 @@ package gameTools;
  * @author Filip Charouzd
  */
 public class Card {
+    //data
+    private String value;
+    private String symbol;
+    private int points;
+
+
+    //konstruktor
+    public Card(String value, String symbol, int points){
+        this.value = value;
+        this.symbol = symbol;
+        this.points = points;
+    }
     
-}
+    public String getValue(){
+        return value;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+    @Override
+    public String toStr
+@Override
+    public String toString() {
+        return "Card [symbol=" + symbol + ", value=" + value + "]";
+    }
